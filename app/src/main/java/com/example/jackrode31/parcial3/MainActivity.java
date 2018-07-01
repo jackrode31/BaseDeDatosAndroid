feb1dd69c87992672f2078673d88af4b3f91dcc6
Roderik Acevedo <rode31x@gmail.com>
2018-06-30 22:55:47 -0500

    addbooks

--- app/src/main/java/com/example/jackrode31/parcial3/MainActivity.java
+++ app/src/main/java/com/example/jackrode31/parcial3/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.jackrode31.parcial3;
 
+import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    ConstraintLayout layout;
+Layout layoutt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        layout = findViewById(R.id.layout);
+
+        layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddBook.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
