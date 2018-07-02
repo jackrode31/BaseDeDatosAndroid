9885993510c36410c1157dcdbfe3605c2a3506d6
Roderik Acevedo <rode31x@gmail.com>
2018-07-01 20:13:11 -0500

    main activity

--- app/src/main/java/com/example/jackrode31/parcial3/MainActivity.java
+++ app/src/main/java/com/example/jackrode31/parcial3/MainActivity.java
@@ -6,15 +6,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Layout;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    ConstraintLayout layout;
-Layout layoutt;
+
+    Button add, update, delete, search, list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        add = findViewById(R.id.btnadd);
+        update = findViewById(R.id.btnupdate);
+        delete = findViewById(R.id.btndel);
+        search = findViewById(R.id.btnsearch);
+        list = findViewById(R.id.btnall);
 
 
     }
